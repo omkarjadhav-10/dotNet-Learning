6b157a33954d65f1a406a7370a6dba1fe573972a
Omkar Jadhav <omkar.jadhav@nonstopio.com>
2026-02-10 13:23:02 +0530

    Minor changes in dependencies

--- obj/Debug/net10.0/DotNET.AssemblyInfo.cs
+++ obj/Debug/net10.0/DotNET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b34ec7c354dc9624ed52fefe52397b3d32773c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

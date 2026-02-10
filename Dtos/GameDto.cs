29b34ec7c354dc9624ed52fefe52397b3d32773c
Omkar Jadhav <omkar.jadhav@nonstopio.com>
2026-02-10 12:54:11 +0530

    Setup basic .NET project environment

---
+++ Dtos/GameDto.cs
@@ -0,0 +1,7 @@
+namespace DotNET.Dtos;
+
+public record class GameDto(int Id,
+                            string Name,
+                            string Genre,
+                            decimal Price,
+                            DateOnly ReleaseDate);
